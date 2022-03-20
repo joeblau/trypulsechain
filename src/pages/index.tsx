f2ccb887dde772d3bfc4c151d0ed8785f69f9e72
Joe Blau <im@joeblau.com>
2022-03-19 21:48:31 -0700

    Add pulsechain url

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -250,7 +250,7 @@ const Home: NextPage = () => {
         <div className="rounded-md shadow">
           <Link href="https://pulsechain.com">
             <a
-              href="#"
+              href="https://pulsechain.com/"
               className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
             >
               Learn more
